b304467a69a651311423c66e88f1cc5d5615b0a7
lazizbeksultonov <lazizbeksultonov0804@gmail.com>
2023-03-28 23:57:51 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import Nav from "./components/Nav.jsx";
+import Jumbotron from "./components/Jumbotron.jsx";
+import SoundSection from "./components/SoundSection.jsx";
+import DisplaySection from "./components/DisplaySection.jsx";
+import WebgiViewer from "./components/WebgiViewer.jsx";
 
 function App() {
 
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    return (
+        <div className="App">
+            <Nav/>
+            <Jumbotron/>
+            <SoundSection />
+            <DisplaySection />
+            <WebgiViewer/>
+        </div>
+    );
 }
 
 export default App;
